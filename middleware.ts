642ad3579e909d5e136c197c6b9938bfe12a3811
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-11-05 03:23:38 +0400

    add: middleware

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+async function redirect(request: NextRequest, url: string) {
+  return NextResponse.redirect(new URL(url, request.nextUrl));
+}
+
+async function checkAuth() {
+  const cookieStore = cookies();
+  const auth = cookieStore.get("session");
+  return auth?.value == null;
+}
+
+const allowedPathsIfNotAuth = ["/auth/login", "/auth/register"];
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const isAuth = await checkAuth();
+
+  if (!isAuth && !allowedPathsIfNotAuth.includes(pathname)) {
+    return redirect(request, "/auth/login");
+  }
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
